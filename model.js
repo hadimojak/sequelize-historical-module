cc14aa133e9eefdde4b6d9a35d36554de258d70a
Hadi Arbabi <h.arbabi@cobex.ir>
2021-09-25 12:20:48 +0330

    update bug for same key values fixed

--- model.js
+++ model.js
@@ -1,112 +1,102 @@
-const { sequelize, DataTypes, Sequelize, Model } = require("./sequelize");
+const { sequelize, DataTypes, Sequelize, Model } = require('./sequelize');
 
-//for example i create User and Product models and although UserHistory and ProductHistory models and
+//for example i create User and Product models and although UserHistory and ProductHistory models and 
 //id (primary key) are build automaticly with sequelize
-class User extends Model {}
-User.init(
-  {
+class User extends Model { };
+User.init({
     firstName: {
-      type: DataTypes.STRING,
-      allowNull: false,
+        type: DataTypes.STRING,
+        allowNull: false,
+    }, lastName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    createdAt: Sequelize.DATE,
+    updatedAt: Sequelize.DATE,
+},
+    {
+        sequelize: sequelize,
+        modelName: "user", paranoid: true
+    }
+);
+
+class UserHistory extends Model { };
+UserHistory.init({
+    user_id: { type: DataTypes.INTEGER },
+    firstName: {
+        type: DataTypes.STRING,
+        allowNull: true
     },
     lastName: {
-      type: DataTypes.STRING,
-      allowNull: false,
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    ip: { type: DataTypes.STRING },
+    opration: { type: DataTypes.STRING },
+    platform: { type: DataTypes.STRING },
+    updatedAt: {
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+    },
+    restoredAt: {
+        type: Sequelize.DATE,
+        defaultValue: null
+    }
+}, {
+    sequelize: sequelize, timestamps: false,
+    modelName: "userHistory"
+});
+
+class Product extends Model { };
+Product.init({
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    }, price: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }, store: {
+        type: DataTypes.INTEGER,
+        allowNull: false
     },
     createdAt: Sequelize.DATE,
     updatedAt: Sequelize.DATE,
-  },
-  {
+}, {
     sequelize: sequelize,
-    modelName: "user",
-    paranoid: true,
-    freezeTableName: true,
-  }
+    modelName: "product", paranoid: true
+}
 );
 
-// class UserHistory extends Model { };
-// UserHistory.init({
-//     user_id: { type: DataTypes.INTEGER },
-//     firstName: {
-//         type: DataTypes.STRING,
-//         allowNull: true
-//     },
-//     lastName: {
-//         type: DataTypes.STRING,
-//         allowNull: true
-//     },
-//     ip: { type: DataTypes.STRING },
-//     opration: { type: DataTypes.STRING },
-//     platform: { type: DataTypes.STRING },
-//     updatedAt: {
-//         type: Sequelize.DATE,
-//         defaultValue: new Date()
-//     },
-//     restoredAt: {
-//         type: Sequelize.DATE,
-//         defaultValue: null
-//     }
-// }, {
-//     sequelize: sequelize, timestamps: false,
-//     modelName: "userHistory"
-// });
-
-class Product extends Model {}
-Product.init(
-  {
+class ProductHistory extends Model { };
+ProductHistory.init({
+    product_id: { type: DataTypes.INTEGER },
     title: {
-      type: DataTypes.STRING,
-      allowNull: false,
+        type: DataTypes.STRING,
     },
     price: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
+        type: DataTypes.INTEGER,
+    }, store: {
+        type: DataTypes.INTEGER,
+        allowNull: true
     },
-    store: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
+    ip: { type: DataTypes.STRING },
+    opration: { type: DataTypes.STRING },
+    platform: { type: DataTypes.STRING },
+    updatedAt: {
+        type: Sequelize.DATE,
+        defaultValue: new Date()
     },
-    createdAt: Sequelize.DATE,
-    updatedAt: Sequelize.DATE,
-  },
-  {
-    sequelize: sequelize,
-    modelName: "product",
-    paranoid: true,
-    freezeTableName: true,
-  }
-);
+    restoredAt: {
+        type: Sequelize.DATE,
+        defaultValue: null
+    }
+}, {
+    sequelize: sequelize, timestamps: false,
+    modelName: "productHistory"
+});
 
-// class ProductHistory extends Model { };
-// ProductHistory.init({
-//     product_id: { type: DataTypes.INTEGER },
-//     title: {
-//         type: DataTypes.STRING,
-//     },
-//     price: {
-//         type: DataTypes.INTEGER,
-//     }, store: {
-//         type: DataTypes.INTEGER,
-//         allowNull: true
-//     },
-//     ip: { type: DataTypes.STRING },
-//     opration: { type: DataTypes.STRING },
-//     platform: { type: DataTypes.STRING },
-//     updatedAt: {
-//         type: Sequelize.DATE,
-//         defaultValue: new Date()
-//     },
-//     restoredAt: {
-//         type: Sequelize.DATE,
-//         defaultValue: null
-//     }
-// }, {
-//     sequelize: sequelize, timestamps: false,
-//     modelName: "productHistory"
-// });
-
-//initialize a simple association
+//initialize a simple association 
 User.hasMany(Product);
 Product.belongsTo(User);
 
-module.exports = { User, Product };
+module.exports = { User, Product, UserHistory, ProductHistory };
